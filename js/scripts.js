e8e56f40ded216e119397f856965a5849e04079e
NoeVivar <nvivar@vcd-ceb131-11.eastern.ewu.edu>
2017-02-22 09:32:09 -0800

    Randomize Image

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 // YOUR SCRIPTS GO HERE
+//storeimage
+
+//storeURL
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+// Get Random Number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+//show images
+$('img').attr('src', eval('img' + numRand));
+
