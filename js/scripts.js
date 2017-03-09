14581d3d62b8ed9e01ab453c6c89f5b850fb7532
NoeVivar <nvivar@it-mmc16-m03.eastern.ewu.edu>
2017-03-09 11:10:30 -0800

    complete overhaul
    back to the beginning

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
-// YOUR SCRIPTS GO HERE
-//storeimage
-
-//storeURL
+// Store 3 images
 var img0 = '../img/200.jpg';
 var img1 = '../img/300.jpg';
 var img2 = '../img/400.jpg';
 
-// Get Random Number
+// Get random number
 var max = 3;
 var numRand = Math.floor( Math.random() * max );
 console.log(numRand);
 
-//show images
-//$('img').attr('src', eval('img' + numRand));
+// Display randomized images
+$('#random').attr( 'src', eval('img' + numRand) );
 
